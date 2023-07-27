ec7cb1760b4383231f95bbe14a051e27d9600579
inomata137 <inomata-kazuki137@g.ecc.u-tokyo.ac.jp>
2023-07-27 23:08:51 +0900

    4.ex -i command option

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,12 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        let mut case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        for arg in args {
+            if *arg == "-i" {
+                case_sensitive = false;
+            }
+        }
 
         Ok(Config { query, filename, case_sensitive })
     }
